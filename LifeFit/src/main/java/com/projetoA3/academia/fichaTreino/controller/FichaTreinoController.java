089a50e328f66d9ce971e585baa9f5c7f1d24c73
Pablo Ernesto <pablo.guerreiro0308@hotmail.com>
2025-10-15 14:13:47 -0300

    feat: implementação do crud da ficha de treino
    Implementação do CRUD da ficha de treino.

---
+++ LifeFit/src/main/java/com/projetoA3/academia/fichaTreino/controller/FichaTreinoController.java
@@ -0,0 +1,47 @@
+package com.projetoA3.academia.fichaTreino.controller;
+
+import com.projetoA3.academia.fichaTreino.entity.FichaTreino;
+import com.projetoA3.academia.fichaTreino.entity.ItemTreino;
+import com.projetoA3.academia.fichaTreino.service.FichaTreinoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fichas-treino")
+public class FichaTreinoController {
+
+    private final FichaTreinoService fichaTreinoService;
+
+    public FichaTreinoController(FichaTreinoService fichaTreinoService) {
+        this.fichaTreinoService = fichaTreinoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FichaTreino> create(@RequestBody FichaTreino fichaTreino) {
+        FichaTreino novaFicha = fichaTreinoService.criarFicha(fichaTreino);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaFicha);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FichaTreino> findById(
+            @PathVariable Long id,
+            @RequestParam(required = false) String treino) {
+        FichaTreino ficha = fichaTreinoService.buscarFichaPorId(id, treino);
+        return ResponseEntity.ok(ficha);
+    }
+
+    @PostMapping("/{id}/progredir")
+    public ResponseEntity<FichaTreino> progredirTreino(@PathVariable Long id) {
+        FichaTreino fichaAtualizada = fichaTreinoService.obterEProgredirTreino(id);
+        return ResponseEntity.ok(fichaAtualizada);
+    }
+    @GetMapping("/{id}/treino-atual")
+    public ResponseEntity<List<ItemTreino>> getTreinoAtual(@PathVariable Long id) {
+ 
+        List<ItemTreino> itensDoDia = fichaTreinoService.buscarProximoTreino(id);
+        return ResponseEntity.ok(itensDoDia);
+    }
+}
